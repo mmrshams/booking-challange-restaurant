ee3d33e985056ff08fbbcfc3fc5962925fcafd7f
mmrshams96 <mmrshams96@gmail.com>
2022-01-06 17:09:14 +0330

    feat: check reserved reservations!

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import * as uuid from "uuid"
 import _ from "lodash";
 import { Config } from "src/common/interfaces/config.interface";
@@ -12,14 +12,33 @@ export class ReservationService {
     private readonly reservationRepository: ReservationRepository,
   ) { }
   async create(data: ReservationInterface): Promise<any> {
+    /** 
+   * NOTE (TODO):
+   * in this part we query to check another reservation exists or not and 
+   * in further steps we can add queue collection and add reservation to queue
+   * if the this time has been reserved !
+   * for this step its not implemented
+   * we only throw some error
+   */
+  const { tableId, bookedTime}= data
+    const currentReservation = await this.reservationRepository.findOne({ tableId, bookedTime });
+    if (currentReservation)
+    throw new HttpException(
+      {
+        status: HttpStatus.NOT_ACCEPTABLE,
+        error: "This time has been reserved!",
+      },
+      HttpStatus.NOT_FOUND
+    );
+
     const refUuid = uuid.v4();
     await this.reservationRepository.createOne({
-      ...data, 
-      uuid:  refUuid, 
-      createdAt: new Date(), 
+      ...data,
+      uuid: refUuid,
+      createdAt: new Date(),
       updatedAt: new Date()
     });
-    return  this.reservationRepository.findOne({ uuid: refUuid });
+    return this.reservationRepository.findOne({ uuid: refUuid });
   }
   async get(id: string): Promise<any> {
     const reservation = await this.reservationRepository.findOne({ uuid: id });

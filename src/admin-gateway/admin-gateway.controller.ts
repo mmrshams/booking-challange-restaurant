6066dd2760c8e0b6b4aa5fa5bf37b4f94ed392f8
mmrshams96 <mmrshams96@gmail.com>
2022-01-07 12:17:29 +0330

    feat: proxy APIs on admin gateway

--- src/admin-gateway/admin-gateway.controller.ts
+++ src/admin-gateway/admin-gateway.controller.ts
@@ -4,21 +4,24 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Param,
   Post,
 } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { User } from "@sentry/node";
+import { ReservationInterface } from "src/common/interfaces/reservations.interface";
+import { ReservationSettingInterface } from "src/common/interfaces/restaurantSetting.interface";
 import { TableInterface } from "src/common/interfaces/table.interface";
 import { AdminGatewayService } from "./admin-gateway.service";
 
 @ApiTags("AdminGateway")
 @Controller("admin-gateway/v1")
 export class AdminGatewayController {
-  constructor(private readonly userService: AdminGatewayService) {}
+  constructor(private readonly adminGatewayService: AdminGatewayService) {}
 
-  @Post("/")
-  async create(@Body() body: TableInterface): Promise<User | null> {
-    const result = await this.userService.create(body);
+  @Post("/create-table")
+  async createTable(@Body() body: TableInterface): Promise<User | null> {
+    const result = await this.adminGatewayService.createTable(body);
     if (!result)
       throw new HttpException(
         {
@@ -30,9 +33,37 @@ export class AdminGatewayController {
     return result;
   }
 
-  @Get("/")
-  async get(): Promise<User | null> {
-    const result = await this.userService.get();
+  @Post("/create-reservation-setting")
+  async createReservationSetting(@Body() body: ReservationSettingInterface): Promise<User | null> {
+    const result = await this.adminGatewayService.createReservationSetting(body);
+    if (!result)
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: "user with id not found!",
+        },
+        HttpStatus.NOT_FOUND
+      );
+    return result;
+  }
+
+  @Post("/create-reservation")
+  async createReservation(@Body() body: ReservationInterface): Promise<User | null> {
+    const result = await this.adminGatewayService.createReservation(body);
+    if (!result)
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: "user with id not found!",
+        },
+        HttpStatus.NOT_FOUND
+      );
+    return result;
+  }
+
+  @Get("/reservation/:uuid")
+  async get(@Param('uuid') uuid: string): Promise<User | null> {
+    const result = await this.adminGatewayService.getReservationWithId(uuid);
     if (!result)
       throw new HttpException(
         {
--- src/admin-gateway/admin-gateway.service.ts
+++ src/admin-gateway/admin-gateway.service.ts
@@ -1,23 +1,73 @@
+import { HttpService } from "@nestjs/common";
 import { Injectable } from "@nestjs/common";
 import _ from "lodash";
 import { Config } from "src/common/interfaces/config.interface";
+import { ReservationInterface } from "src/common/interfaces/reservations.interface";
+import { ReservationSettingInterface } from "src/common/interfaces/restaurantSetting.interface";
 import { TableInterface } from "src/common/interfaces/table.interface";
-import { TableRepository } from "src/common/repositories/table.repository";
-import { UserRepository } from "src/common/schemas/user.entity";
+
 @Injectable()
 export class AdminGatewayService {
   constructor(
     private readonly config: Config,
-    // private readonly userRepository: UserRepository,
-    // private readonly dogRepository: TableRepository
-  ) {}
-  async create(data: TableInterface): Promise<any> {
-    // await this.dogRepository.createOne({ name: "some specific dog name" });
-    // return this.userRepository.create(data);
+    private httpService: HttpService,
+  ) { }
+
+  /** 
+    * NOTE:
+    * only proxy the back-tire APIs for now
+    */
+  async createReservationSetting(data: ReservationSettingInterface): Promise<any> {
+    const result = await this.httpService
+      .post(
+        this.config.HTTP_INVENTORY_URI + '/reservationSetting',
+        {
+          ...data
+        },
+        {
+          timeout: 50000,
+        }
+      )
+      .toPromise();
+
+    return result.data;
   }
-  async delete(id: string, data: TableInterface): Promise<any> {}
-  async update(id: string, data: TableInterface): Promise<any> {}
-  async get(): Promise<any> {
-    return { Id: 123, name: "boilerplate" };
+  async createReservation(data: ReservationInterface): Promise<any> {
+    const result = await this.httpService
+      .post(
+        this.config.HTTP_RESERVATION_URI + '/',
+        {
+          ...data
+        },
+        {
+          timeout: 50000,
+        }
+      )
+      .toPromise();
+
+    return result.data;
+  }
+  async createTable(data: TableInterface): Promise<any> {
+    const result = await this.httpService
+      .post(
+        this.config.HTTP_INVENTORY_URI + '/table',
+        data,
+        {
+          timeout: 50000,
+        }
+      )
+      .toPromise();
+
+    return result.data;
+  }
+
+  async getReservationWithId(uuid: string): Promise<any> {
+    const result = await this.httpService
+      .get(
+        this.config.HTTP_RESERVATION_URI + `/${uuid}`,
+      )
+      .toPromise();
+
+    return result.data;
   }
 }
